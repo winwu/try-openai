7a1b762b70cc4ebee11ab0763ed837a925bbc2db
winwu <look_me08@hotmail.com>
2023-06-11 16:25:52 -0700

    first commit

---
+++ 01_first_try.py
@@ -0,0 +1,9 @@
+import os
+import openai
+import tiktoken
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+print(os.environ['OPENAI_API_KEY'])
---
+++ README.md
@@ -0,0 +1,14 @@
+# Try openai
+
+the repo is my practice playground for learning openai api
+
+## environment
+
+I use python3 and my pip version is 23.1.2
+
+```
+pip3 install tiktoken
+pip3 install load_dotenv
+pip3 install openai
+```
+
